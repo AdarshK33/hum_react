bf8d0aca88d94a393aa4c4643ef44df91a79cc91
Nikhil Bhagath <nikhil@theretailinsights.com>
2020-10-03 00:36:57 +0530

    cookie secure set to false

--- src/utils/cookies.js
+++ src/utils/cookies.js
@@ -1,13 +1,13 @@
 import Cookies from "js-cookie";
 
 const secure = process.env.NODE_ENV === 'production'
-
+console.log({secure})
 Cookies.defaults = {
   path: "/",
 //   domain: ".example.com",
   expires: 30,
   sameSite: 'lax',
-  secure
+  secure: false
 };
 
 export default Cookies;
