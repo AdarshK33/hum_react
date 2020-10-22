da35fe5b409b2d1302b5aa7039ee5ea078f894b1
ranjithkumarhnRetailInsight <ranjith@theretailinsights.com>
2020-10-22 19:29:57 +0530

    app state

--- src/context/AppState.js
+++ src/context/AppState.js
@@ -95,7 +95,7 @@ export const AppProvider = ({ children, history }) => {
       .catch((error) => {
         console.log(error)
         setTimeout(function () { userLogout(); }, 4000);
-
+        // 4 seconds
         toast.error("User does not exist");
       })
   }
