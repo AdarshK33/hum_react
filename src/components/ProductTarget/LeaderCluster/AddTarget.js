6bd42ad4873eb96bce16db7cb81c79d9b21ea011
Navaneetha Varier <navaneetha@theretailinsights.com>
2020-11-01 12:54:44 +0530

    Add Target in Cluster Leader bug fixed

--- src/components/ProductTarget/LeaderCluster/AddTarget.js
+++ src/components/ProductTarget/LeaderCluster/AddTarget.js
@@ -29,7 +29,7 @@ const AddTarget = (props) => {
 
     const { viewCostCentre } = useContext(DashboardContext);
     const { user } = useContext(AppContext)
-    const { leaderClusterList, viewLeaderClusterList,addLeaderTarget} = useContext(ClusterProductContext);
+    const { leaderClusterList, viewLeaderClusterList,addTarget} = useContext(ClusterProductContext);
 
 
      useEffect(() => {
@@ -106,7 +106,7 @@ const AddTarget = (props) => {
             targetId: 0,
             year: year
           }
-          addLeaderTarget(Values);
+          addTarget(Values);
 
         const setModal = props.handleClose;
 
