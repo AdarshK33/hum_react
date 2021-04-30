a806035e0e8375f5ea47f2f4bef8e68e71ec7dac
sachinN <sachin.nagalikar@theretailinsights.com>
2021-04-30 11:56:23 +0530

    f and f listing

--- src/components/CandidateVerification/ManageCandidates.js
+++ src/components/CandidateVerification/ManageCandidates.js
@@ -175,8 +175,9 @@ const CandidateList = () => {
 
                       <td>
                         {/* <Link to={"/verification/" + item.candidateId}> */}
-                               {/* <Link to={"/admin_no_due_clearance"}> */}
-                          <Link to={"/no_due_clearance"}>
+                               <Link to={"/admin_no_due_clearance"}>
+                              {/* <Link to={"/admin-finance-clearance"}> */}
+                          {/* <Link to={"/no_due_clearance"}> */}
                           {/* <Link to={"/finance-clearance"}> */}
                           <Edit2
                             onClick={() => {
