09c8a77c7fab31aae0d6e5c93051ba25de159951
adkumar <adarsh.kumar@bnedloudcloud.com>
2022-07-16 21:04:05 +0530

    #16 changes on doc list table body

--- src/components/DocumentManagement/DocsListTableBody.js
+++ src/components/DocumentManagement/DocsListTableBody.js
@@ -31,6 +31,7 @@ const DocsListTableBody = ({ docsList,EmployeeId }) => {
   const handleDocClose = () => {
     setDocShow(false);
   };
+  console.log("docsList",docsList)
 
   return (
     <Fragment>
@@ -54,27 +55,39 @@ const DocsListTableBody = ({ docsList,EmployeeId }) => {
       )}
     </Modal.Body>
   </Modal>
-  {docsList.map((doc, index) => (
-    <tr>
-      <td>{index+1}</td>
-      <td>{doc.documents[0]}</td>
-      <td>
-        {/* <a href={process.env.REACT_APP_S3_URL + doc} target="_blank">
-          <Eye  />
-        </a> */}
-        {/* <a href={imageViewData.data ? imageViewData.data:""} target="_blank"> */}
-          <Eye onClick={()=>{ImageView(doc.documents[0],EmployeeId);handleDocShow()}} />
-        {/* </a> */}
-      </td>
-      <td>
+  {docsList.map((doc, index) =>{ 
+    console.log("doc--", doc)
+    return(
+   <>
+      {doc.documents.map((list, index) =>{ 
+        console.log("lllllll",list)
+        return (
+          <>
+          <tr>  <td>{index+1}</td>
+        
+         <td>{list}</td>
+         <td>
+         <a href={process.env.REACT_APP_S3_URL + list} target="_blank">
+           <Eye  />
+         </a> 
+         {/* {/* <a href={imageViewData.data ? imageViewData.data:""} target="_blank"> */}
+           <Eye onClick={()=>{ImageView(list,EmployeeId);handleDocShow()}} /> 
+         {/* </a> */}
+       </td>
+       <td>
         <Download
-          data-doc={doc.documents[0]}
+          data-doc={list}
           onClick={downloadDoc}
           style={{ color: "#007bff", cursor: "pointer" }}
         />
       </td>
-    </tr>
-    ))}
+      </tr>
+       </>
+      )})}
+    
+    </>
+   
+    )})}
     </Fragment>
   );
 };
