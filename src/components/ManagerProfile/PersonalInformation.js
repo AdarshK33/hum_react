5b751adc53f2bedb12cca1d4a338dd9fb1dfa172
ADARSH KUMAR <adarsh@theretailinsights.com>
2022-11-22 17:19:39 +0530

    #57 worked on payrol search & my profile email & generate btn

--- src/components/ManagerProfile/PersonalInformation.js
+++ src/components/ManagerProfile/PersonalInformation.js
@@ -85,7 +85,8 @@ const PersonalInformation = (props) => {
   };
   const emailValidation = () => {
     const emailValid =
-      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+ 
     if (
       personalEmailId !== "" &&
       personalEmailId !== null &&
